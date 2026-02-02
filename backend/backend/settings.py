d8008829d47dfb02e20363763bd6141e4845c45a
Jawad Shaikh <shaikhjawwad8@gmail.com>
2026-02-02 12:22:21 +0530

    added whitenoise dependencies for deployment on render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-#en-&=4+*=)4uuz!a#v8f2u4z0kce3dx*r5&03*ef8ej7t%&s0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,5 +122,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
